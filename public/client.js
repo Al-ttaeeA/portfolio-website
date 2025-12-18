12496d2c1e57e13d6f88471ebbd4d95c54964bb4
Al-ttaeeA <ahmedalttaee0306@gmail.com>
2025-12-17 20:12:00 -0500

    Refactored repo to statically host the website

--- public/client.js
+++
@@ -1,9 +0,0 @@
-const socket = io(); // same origin
-
-socket.on('connect', () => {
-    console.log('connected: ' + socket.id);
-});
-
-socket.on('message', (data) => {
-    console.log('server → ' + JSON.stringify(data));
-});
